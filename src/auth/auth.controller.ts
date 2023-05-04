e5cc8fd6387e8084960ac5c79d2a078b2601c434
Fabrice <codinglone@gmail.com>
2023-05-04 08:52:25 +0200

    Added basic endpoints

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
 
-@Controller()
+@Controller('auth')
 
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService){
+        
+    }
+    @Post('signup')
+    signup(){
+        return 'I am signed up'
+    }
+    @Post('signin')
+    signin(){
+        return 'I am signin in'
+    }
+}
