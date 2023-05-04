435bfa4290e52dfac30fe6b0a6e6c55fcc0d5c3f
Fabrice <codinglone@gmail.com>
2023-05-04 15:16:24 +0200

    Added prisma datasources connection

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+    constructor(){
+        super({
+            datasources: {
+                db: {
+                    url: 'postgresql://postgres:123@localhost:5434/nest?schema=public'
+                }
+            }
+        })
+    }
+}
