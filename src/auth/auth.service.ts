520dabb33eff1c6a3d77ea059ade8ee55441c878
Fabrice <codinglone@gmail.com>
2023-05-04 15:30:49 +0200

    Modified prisma module to be available globally

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 // import { User, Bookmark } from "@prisma/client"
 @Injectable()
 
 export class AuthService {
+    constructor(private prisma: PrismaService) {
+        
+    }
     signup(){
         return { message: "I have signed up" }
     }
