f944adf318272f04e47907515ee1aced0bfd8608
Fabrice <codinglone@gmail.com>
2023-05-04 16:21:05 +0200

    Configured white list to the useGlobalPipes to accept only the specified fields in dto

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 require('dotenv').config()
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }))
   await app.listen(process.env.PORT);
 }
 bootstrap();
